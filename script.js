d06e80178312d4d0de77cf136805f52fb6f65179
Tatiana-Juin <tatiana.juinp15@gmail.com>
2024-01-20 14:36:57 +0100

    creation de la fonction calculer qui retourne le resultat d'un calcule

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 // RECUPERE LES ELEMENTS DU DOM
 let ecran = document.querySelector(".ecran");
-//SELECTIONNE ELEMENT ET ON LE TRANSFORME EN TABLEAU GRACE AU SPRITE
+//SELECTIONNE ELEMENT ET ON LE TRANSFORME EN TABLEAU 
 let touches = [...document.querySelectorAll(".btn")];
 
 // console.log(touches);
+
+let precedent = 0; 
+let actuel=0;
+
+// Pour récupérer toutes les touches
+for(let touche of touches){
+    // console.log(touche);
+    touche.addEventListener("click",gererTouche)
+    // touche.addEventListener("keydown",gererTouche)
+}
+
+
+// GERER LES TOUCHES
+function gererTouche(){
+    // récupérer la valeur des touches 
+    let touche = this.innerText;
+}
+
+/**
+ * 
+ * @param {number} nb1 
+ * @param {number} nb2 
+ * @param {string} operateur 
+ * @returns number
+ */
+function calculer(nb1,nb2,operateur){
+    if(operateur ==="/") return nb1 / nb2;
+    if(operateur === "*" ) return nb1 * nb2;
+    if(operateur ==="-" ) return nb1 - nb2;
+    if(operateur ==="+") return nb1 + nb2;
+}
+
 // POUR RECUPERE TOUTES LES KEY DE CHAQUE TOUCHE 
 // let listKeyCodes = touches.map(touche=> touche.dataset.key);
 // console.log(listKeyCodes);
